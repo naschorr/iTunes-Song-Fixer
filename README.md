5075bfafe40ad5d146d90375d64798dd637a5fa3
Nick Schorr <naschorr@gmail.com>
2016-05-28 18:45:44 -0400

    Add support for different directories, and improved output formatting.
    Added support for executing in different directories with the
    --directory option.
    Added support for deleting songs with low bitrates using the
    --min-bitrate option.
    Changed output to be more informative, and use less lines.
    Made any unhandled exceptions more descriptive.

--- README.md
+++ README.md
@@ -3,15 +3,17 @@ Unobfuscates MP3 file names that've been renamed by iTunes
 
 ![Example output](https://raw.githubusercontent.com/naschorr/iTunes-Song-Fixer/master/images/example_output.png)
 
-> Some example output taken from an ancient iPod.
+> Some example output taken from songs recovered from an ancient iPod.
 
 
-Basically, it'll turn that wall of MP3s with four-letter names into a wall of MP3s with names that make sense. It does this via the handy [Mutagen](https://mutagen.readthedocs.io/en/latest/#) module, and gives you a few options for formatting the output. It's important to note that this program only reads the tag data, so nothing is permanant.
+Basically, it'll turn that wall of MP3s with four-letter names into a wall of MP3s with names that make sense. It does this via the handy [Mutagen](https://mutagen.readthedocs.io/en/latest/#) module, and gives you a few options for formatting the output. It's important to note that this program only *reads* the tag data, so nothing is permanant.
 
-The `--delete` flag will automatically delete MP3s with broken or nonexistent tags. This usually happens when Mutagen can't load in the ID3 data. <br />
-The `--title-artist` flag changes the renamed files to have the title of the song before the artist, instead of the other way around (`--artist-title`) by default. <br />
-The `--title-only` flag sets the renamed files to only have a title.
+The `--delete` flag will automatically delete MP3s with broken or nonexistent tags. This usually happens when Mutagen can't load in the ID3 data. Defaults to '--no-delete'.<br />
+The `--title-artist` flag changes the renamed files to have the title of the song before the artist. Defaults to `--artist-title`. <br />
+The `--title-only` flag sets the renamed files to only have a title. Defaults to '--both'. <br />
+The '--min-bitrate' or '-b' option lets you select a required minimum bitrate (in Kbps) for songs. It will delete songs that don't fulfil this. Defaults to 0 Kbps when not specified. <br />
+The '--directory' or '-d' option lets you select a different working directory for the program to execute in. Defaults to the current working directory if not specified. Requires a fully qualified pathname if used.
   
-Note: This requires [Click](http://click.pocoo.org/5/), and [Mutagen](https://mutagen.readthedocs.io/en/latest/#). You can install them with `pip install -r requirements.txt`. It also *requires* that you put the SongFixer.py program into the directory that contains all of the songs to be fixed.
+Note: This requires [Click](http://click.pocoo.org/5/), and [Mutagen](https://mutagen.readthedocs.io/en/latest/#). You can install them with `pip install -r requirements.txt`.
 
-Another Note: The songs were pulled from a jailbroken iPod running an early version of iOS 3, so no guarantees that this will work on anything later.
+Another Note: The songs were pulled from a jailbroken iPod running an early version of iOS 3, so no guarantees that this will work on anything else.
--- SongFixer.py
+++ SongFixer.py
@@ -1,7 +1,10 @@
 import click
 import os
+import sys
+
 from re import sub
 from mutagen.easyid3 import EasyID3
+from mutagen.mp3 import MP3
 from mutagen.id3._util import ID3NoHeaderError, error
 
 ## Returns true if the supplied file is an MP3
@@ -11,55 +14,71 @@ def isMP3(fileName):
 		return True
 	return False
 
-## Get MP3s in current directory
-def getMP3s():
-	return [fileName for fileName in os.listdir() if os.path.isfile(fileName) and isMP3(fileName)]
+## Get MP3s in given directory
+def getMP3s(directory):
+	return [os.path.join(directory, fileName) for fileName in os.listdir(directory) if os.path.isfile(os.path.join(directory, fileName)) and isMP3(fileName)]
 
 ## Removes special characters from the string
 def sanitizeString(string):
 	return sub(r'[?|$|.|!|/|\\]',r'', string)
 
-def deleteSong(song):
-	print("Deleting " + song)
+## Alerts the user that a song is being removed, and then deletes it.
+def deleteSong(song, explanation=None):
+	output = "Deleting " + song + ". "
+	if(explanation is not None):
+		output += "(" + explanation + ")"
+	print(output)
 	os.remove(song)
+
+## Gets the last part of a path (eg. /dir1/dir2/test returns test)
+def getPathEnd(path):
+	return os.path.basename(os.path.normpath(path))
 
 @click.command()
 @click.option('--delete/--no-delete', default=False, help='Deletes MP3s with malformed tag sets. (eg. No title)')
 @click.option('--title-artist/--artist-title', default=False, help='Choose whether to put the artist or title first in the new name.')
 @click.option('--title-only/--both', default=False, help='Choose whether to keep only the song title or both the artist and title.')
-def main(delete, title_artist, title_only):
-	songs = getMP3s()
+@click.option('--min-bitrate', '-b', default=0, help='Specifies a minimum bitrate that all songs must have, else they get deleted.')
+@click.option('--directory', '-d', default=os.getcwd(), type=click.Path(exists=True), help='Specifies a directory for the program to execute in. Requires a fully qualified pathname.')
+def main(delete, title_artist, title_only, min_bitrate, directory):
+	songs = getMP3s(directory)
 	for song in songs:
 		try:
 			loaded = EasyID3(song)
 			if("title" in loaded and "artist" in loaded):
+				bitrate = MP3(song).info.bitrate / 1000
+				if(min_bitrate >= bitrate):		## Ensure minimum bitrate is reached
+					deleteSong(song, "Bitrate below: " + str(min_bitrate))
+					continue
+
 				title = sanitizeString(str(loaded["title"][0]))
 				renamed = ['','', '', '.mp3']	## Since we're only dealing with mp3s at the moment.
 
-				if(not title_only):		## Thus, we'll have the title and the artist somewhere.
+				if(not title_only):				## Thus, we'll have the title and the artist somewhere.
 					renamed[1] = ' - '
 
-				if(not title_artist): 	## Thus, the artist string comes first
+				if(not title_artist): 			## Thus, the artist string comes first
 					renamed[0] = sanitizeString(str(loaded["artist"][0])) * (not title_only)
 					renamed[2] = title
-				else:					## Thus, the title string comes first
+				else:							## Thus, the title string comes first
 					renamed[0] = title
 					renamed[2] = sanitizeString(str(loaded["artist"][0])) * (not title_only)
 
-				renamed = ''.join(renamed)
+				renamed = os.path.join(directory, ''.join(renamed))
 
 				os.rename(song, renamed)
-				print(song + " ==> " + renamed)
+				print(getPathEnd(song) + " ==> " + getPathEnd(renamed))
 
 		except ID3NoHeaderError:
-			print("ID3NoHeaderError exception for song: " + song)
 			if(delete):
-				deleteSong(song)
+				deleteSong(song, "ID3NoHeaderError exception")
+			continue
 		except error:
-			print("Mutagen Error exepction for song: " + song)
 			if(delete):
-				deleteSong(song)
-		except FileExistsError:
-			print("FileExistsError exception for song: " + song)
+				deleteSong(song, "MutagenError exception")
+			continue
+		except:
+			exception = sys.exc_info()[0].__name__
+			print(exception + " exception for song: " + song)
 
 main()
